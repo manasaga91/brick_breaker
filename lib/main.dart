b8ee651343a5187ec7eaf4e1d4a27f7b70a7fd50
msugawa <msugawa@jops.co.jp>
2024-03-26 11:59:17 +0900

    Display the ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'src/brick_breaker.dart';
+
 void main() {
-  final game = FlameGame();
+  final game = BrickBreaker();
   runApp(GameWidget(game: game));
 }
