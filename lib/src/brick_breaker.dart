9b4bed89a5a7e94e5cee53dc7e5b97e48d84292e
msugawa <msugawa@jops.co.jp>
2024-03-26 20:10:52 +0900

    Get bat on ball

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -2,12 +2,16 @@ import 'dart:async';
 import 'dart:math' as math;
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'components/components.dart';
 import 'config.dart';
 
-class BrickBreaker extends FlameGame with HasCollisionDetection {
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents {
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -38,6 +42,27 @@ class BrickBreaker extends FlameGame with HasCollisionDetection {
       ),
     );
 
+    world.add(
+      Bat(
+        size: Vector2(batWidth, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95),
+      ),
+    );
+
     debugMode = true;
+  }
+
+  @override // Add from here...
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+    return KeyEventResult.handled;
   }
 }
--- lib/src/components/ball.dart
+++ lib/src/components/ball.dart
@@ -1,8 +1,10 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flutter/material.dart';
 
 import '../brick_breaker.dart';
+import 'bat.dart';
 import 'play_area.dart';
 
 class Ball extends CircleComponent
@@ -39,9 +41,16 @@ class Ball extends CircleComponent
       } else if (intersectionPoints.first.x >= game.width) {
         velocity.x = -velocity.x;
       } else if (intersectionPoints.first.y >= game.height) {
-        velocity.y = -velocity.y;
-        // removeFromParent();
+        add(
+          RemoveEffect(
+            delay: 0.35,
+          ),
+        );
       }
+    } else if (other is Bat) {
+      velocity.y = -velocity.y;
+      velocity.x = velocity.x +
+          (position.x - other.position.x) / other.size.x * game.width * 0.3;
     } else {
       debugPrint('collision with $other');
     }
