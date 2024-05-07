da9cf6439c933f307c9fb17c2b668917e6fae8b1
msugawa <msugawa@jops.co.jp>
2024-05-07 18:43:41 +0900

    game over brick

--- lib/src/components/brick.dart
+++ lib/src/components/brick.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:brick_breaker/src/brick_breaker.dart';
+import 'package:brick_breaker/src/components/components.dart';
 import 'package:brick_breaker/src/components/item.dart';
 import 'package:brick_breaker/src/config.dart';
 import 'package:flame/collisions.dart';
@@ -32,22 +33,31 @@ class Brick extends SpriteComponent
     PositionComponent other,
   ) {
     super.onCollisionStart(intersectionPoints, other);
-    removeFromParent();
-    game.score.value += 100;
-    FlameAudio.play('Bluezone_BC0295_sci_fi_weapon_gun_shot_008.wav');
+    if (other is Ball) {
+      removeFromParent();
+      game.score.value += 100;
+      FlameAudio.play('Bluezone_BC0295_sci_fi_weapon_gun_shot_008.wav');
 
-    if (game.rand.nextInt(10) == 0) {
-      game.world.add(
-        Item(
-          position: position,
-        ),
-      );
+      if (game.rand.nextInt(10) == 0) {
+        game.world.add(
+          Item(
+            position: position,
+          ),
+        );
+      }
     }
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    position += Vector2(0, scrollSpeed * dt);
+
+    if (position.y > game.height * 0.85) {
+      game.playState = PlayState.gameOver;
+    }
+
+    if (game.playState == PlayState.playing) {
+      position += Vector2(0, scrollSpeed * dt);
+    }
   }
 }
